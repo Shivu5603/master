f23dac132a40f8b9ef5e27b01514452cf110bf75
Shivu5603 <87187366+Shivu5603@users.noreply.github.com>
2024-09-29 16:19:41 +0530

    Add files via upload

---
+++ lofer.py
@@ -0,0 +1,10 @@
+print("lofer")
+print("hii prends How are you")
+a=int(input("enter the number\n"))
+b=int(5)
+for i in range(b):
+    print(i,0)
+if (a%2==0):
+    print("its a even")
+else:
+    print("its an odd")
